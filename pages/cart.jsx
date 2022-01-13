9e04810cc09a42504d04c274e588d319d4b1e045
Tohir <TOXIR_DEV>
2022-01-13 15:01:14 +0600

    I have finished cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import StyleCart from "../styles/StyleCart";
 
@@ -14,9 +15,50 @@ const Cart = () => {
             <th>Quantity</th>
             <th>Total</th>
           </tr>
+          <tr>
+            <td>
+              <div className="imgContainer">
+                <Image
+                  src="/img/pizza.png"
+                  layout="fill"
+                  objectFit="cover"
+                  alt="pizza.png"
+                />
+              </div>
+            </td>
+            <td>
+              <span className="name">CORALZO</span>
+            </td>
+            <td>
+              <span className="extras">Double ingredient, spicy sauce</span>
+            </td>
+            <td>
+              <span className="price">$19.9</span>
+            </td>
+            <td>
+              <span className="quantity">2</span>
+            </td>
+            <td>
+              <span className="total">$39.80</span>
+            </td>
+          </tr>
         </table>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2 className="title">CART TOTAL</h2>
+          <div className="totalText">
+            <b className="totalTextTitle">Subtotal: </b>$79.60
+          </div>
+          <div className="totalText">
+            <b className="totalTextTitle">Discount: </b>$0.00
+          </div>
+          <div className="totalText">
+            <b className="totalTextTitle">Total: </b>$79.60
+          </div>
+          <button className="button">CHECKOUT NOW!</button>
+        </div>
+      </div>
     </StyleCart>
   );
 };
