76a0a1383d5772ab332ee29254c74bc9e54054db
Tohir <TOXIR_DEV>
2022-01-13 10:50:05 +0600

    I have finished footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,55 @@
+import Image from "next/image";
 import React from "react";
-
+import StyleFooter from "../styles/StyleFooter";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <StyleFooter>
+      <div className="item">
+        <Image src="/img/bg.png" alt="bg.png" objectFit="cover" layout="fill" />
+      </div>
+      <div className="item">
+        <div className="card">
+          <h2 className="motto">
+            OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
+          </h2>
+        </div>
+        <div className="card">
+          <h1 className="title">FIND OUR RESTAURANTS</h1>
+          <p className="text">
+            1654 R. Don Road #304.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className="text">
+            2356 K. Laquie Rd #235.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1011
+          </p>
+          <p className="text">
+            1614 E. Erwin St #104.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1012
+          </p>
+          <p className="text">
+            1614 W. Caroll St #125.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1013
+          </p>
+        </div>
+        <div className="card">
+          <h1 className={"title"}>WORKING HOURS</h1>
+          <p className="text">
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 – 22:00
+          </p>
+          <p className="text">
+            SATURDAY - SUNDAY
+            <br /> 12:00 – 24:00
+          </p>
+        </div>
+      </div>
+    </StyleFooter>
+  );
 };
 
 export default Footer;
